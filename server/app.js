170addfbac9e20f7b3e103ea293a53fd5bfdcf38
jaybamania <jaykumardiu@gmail.com>
2021-04-12 00:55:52 +0530

    Connected-with-MongoDB

--- server/app.js
+++ server/app.js
@@ -1,10 +1,32 @@
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
+
+const DB = 'mongodb+srv://jaybamania:jaybamania@cluster0.mrtro.mongodb.net/mernstack?retryWrites=true&w=majority'
+
+mongoose.connect(DB, { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+        })
+        .then(()=>{
+             console.log(`Connection Successfull`)
+        })
+        .catch((err) => console.log(`no connection`))
+
+//Middleware
+const middleware = (req,res,next)=>{
+    console.log(`Hello World!!`)
+    next()
+}
+
 
 app.get('/',(req,res)=>{
     res.send(`Hello world from the server`)
 })
-app.get('/about',(req,res)=>{
+app.get('/about',middleware,(req,res)=>{
+    console.log('Hello About')
     res.send(`About from the server`)
 })
 app.get('/contact',(req,res)=>{
